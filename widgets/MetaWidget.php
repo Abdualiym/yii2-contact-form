150098908d6b031d0f764a2e5bddbd6d8125f41b
Achilov Davronbek <achilov21@yandex.com>
2018-04-09 13:35:17 +0500

    textMetaFields

--- widgets/MetaWidget.php
+++ widgets/MetaWidget.php
@@ -14,8 +14,8 @@ class MetaWidget extends Widget
 
     public function run()
     {
-        //TextMetaFields::find()->where(['text_id' => $id, 'key'=> $key])->one()->value;
-        $text = TextMetaFields::find()->where(['text_id' => 9])->all();
-        return $this->render('_meta', ['text' => $text]);
+        return $this->render('_meta', [
+            'text' => new TextMetaFields()
+            ]);
     }
 }
--- widgets/views/_meta.php
+++ widgets/views/_meta.php
@@ -8,28 +8,14 @@
 //\yii\helpers\VarDumper::dump($text, 10, true);die();
 ?>
 <address>
-    <?php foreach ($text as $items):
-        if($items->key=="phone"):
-            ?>
-    <a class="contact" href="tel:<?=$items->value; ?>">
+
+    <a class="contact" href="tel:<?= $text->value('phone'); ?>">
         <i class="icon-phone"></i>
-        <?=$items->value; ?>
+        <?= $text->value('phone'); ?>
     </a>
-    <?php endif;
-    endforeach; ?>
-
-
-    <?php
-    foreach ($text as $items):
-        if($items->key=="email"):
-            ?>
-    <a class="contact" href="mailto:<?=$items->value; ?>">
+    <a class="contact" href="mailto:<?= $text->value('email'); ?>">
         <i class="icon-mail"></i>
-        <?=$items->value; ?>
+        <?= $text->value('email'); ?>
     </a>
-    <?php
-        endif;
-    endforeach;
-    ?>
 </address>
 
